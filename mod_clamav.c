ea812f246852f0744a2a6fc5182b677a16d4b347
Joseph Benden <joe@benden.us>
2016-01-22 08:54:33 -0700

    Fix bug where ProFTPD does not prioritize error returns.

--- mod_clamav.c
+++ mod_clamav.c
@@ -113,9 +113,9 @@ static int clamavd_result(int sockd, const char *abs_filename, const char *rel_f
        * virus (only for FTP/FTPS connections.)
        */
       proto = pr_session_get_protocol(0);
-        pr_response_add_err(R_550, "Virus Detected and Removed: %s", pt);
       if (strncmp(proto, "ftp", 3) == 0 ||
           strncmp(proto, "ftps", 4) == 0) {
+        pr_response_send(R_550, "Virus Detected and Removed: %s", pt);
       }
 
       /* Log the fact */
